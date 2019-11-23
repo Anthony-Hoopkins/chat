197abaa5af58b6f285c20ea2b0b73b8fed730ad0
Alex Shinkevich <alex.shynkevich@gmail.com>
2019-11-23 12:25:47 +0300

    chore: add base styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import './styles/index.scss';
 
-const App = () => {
-  return (
-    <div className="App">
-      Hi
-    </div>
-  );
-};
+const App = () => (
+  <div className="App">
+    Hi
+  </div>
+);
 export default App;
