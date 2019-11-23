fc981b0159cb2a1194425c00f1ddc7bdfaf00d07
Alexey Verbitsky <leshaverbitsky@gmail.com>
2019-11-23 14:32:16 +0300

    feat: implement messages service

---
+++ src/services/message.service.js
@@ -0,0 +1,19 @@
+export default class MessageService {
+  constructor() {
+  }
+
+
+  getMessagesList() {
+
+  }
+
+  getConversation() {
+
+  }
+
+  sendMessage() {
+
+  }
+
+  // TODO delete, edit message
+}
